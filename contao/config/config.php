6129ae0f202495bdb61bc470787bc4da47aad5e4
Sven Baumann <baumann.sv@gmail.com>
2015-02-27 10:16:57 +0100

    add backend functions

---
+++ contao/config/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Contao Module Device
+ * Copyright (C) 2015 Sven Baumann
+ *
+ * PHP version 5
+ *
+ * @package   contaoblackforest/contao-module-device
+ * @file      config.php
+ * @author    Sven Baumann <baumann.sv@gmail.com>
+ * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @license   LGPL-3.0+
+ * @copyright ContaoBlackforest 2015
+ */
+
+$GLOBALS['TL_HOOKS']['loadDataContainer'][] = array('ContaoBlackforest\Backend\DCA\Module\Device', 'addChildRecordCallback');
+$GLOBALS['TL_HOOKS']['parseWidget'][]       = array('ContaoBlackforest\Backend\DCA\Module\Device', 'addVisibleDeviceToWizard');
